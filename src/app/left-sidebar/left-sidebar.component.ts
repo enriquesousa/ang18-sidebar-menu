57c09a22aa4f7aa0ed241c8b6093014b24de0215
Enrique <enrique.sousa@gmail.com>
2025-10-20 11:08:59 -0700

    02. Routing Rendering Links

--- src/app/left-sidebar/left-sidebar.component.ts
+++ src/app/left-sidebar/left-sidebar.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-left-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './left-sidebar.component.html',
   styleUrl: './left-sidebar.component.css',
 })
-export class LeftSidebarComponent {}
+export class LeftSidebarComponent {
+  items = [
+    {
+      routeLink: 'dashboard',
+      icon: 'fal fa-home',
+      label: 'Dashboard',
+    },
+    {
+      routeLink: 'products',
+      icon: 'fal fa-box-open',
+      label: 'Products',
+    },
+    {
+      routeLink: 'pages',
+      icon: 'fal fa-file',
+      label: 'Pages',
+    },
+    {
+      routeLink: 'settings',
+      icon: 'fal fa-cog',
+      label: 'Settings',
+    },
+  ];
+
+}
