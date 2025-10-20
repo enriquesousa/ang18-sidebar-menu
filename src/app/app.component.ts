3046fd4279e47b53e5abe6e0044a6ae13e2c9b48
Enrique <enrique.sousa@gmail.com>
2025-10-20 10:45:41 -0700

    01. Install Ang18 y Crear Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [LeftSidebarComponent, MainComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ang18-sidebar-menu';
